35344849250f946d061891a789e6d380d9e3e463
yezhuming <yzming0769@qq.com>
2019-08-12 15:15:01 +0800

    第一次提交

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="footer">
+        <p>Copyright © 2019【陈旭涛 20162180110】【叶柱铭 20162180203】</p>    
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.footer {
+    font-size: 14px;
+    text-align: center;
+    background-color: #545c64;
+    color: #fff;
+}
+.footer p {
+    height: 100px;
+    line-height: 100px;
+    margin: 0;
+}
+</style>
